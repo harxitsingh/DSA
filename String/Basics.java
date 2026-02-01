f4ba264d857222bd5e868c8ce88cb2e57b2bfe95
Harshit Singh <harxitsingh@gmail.com>
2026-02-01 22:11:25 +0530

    String Basics

---
+++ String/Basics.java
@@ -0,0 +1,47 @@
+// // // String 
+
+
+
+public class Basics {
+    public static void main(String[] args) {
+        // char[] arr = { 'p', 'q', 'e', 'r', 's', 't' };
+
+        // // print chracter array
+        // for (char ele : arr) {
+        // System.out.print(ele + " ");
+        // }
+
+        // System.out.println();
+
+        // // print specific character in array
+        // System.out.println(arr[3]); //out - r
+
+        // // }
+        // // }
+
+        // // print string
+        // String s = "shruti is a good girl" ;
+        // System.out.println(s);
+
+        // // print String using user input
+        // import java.util.Scanner;
+        // public class Basics {
+        // public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        // String s = sc.next();// print only single word
+        // System.out.println(s);
+
+        // String t = sc.nextLine(); // print full line
+        // System.out.println(t);
+        // }
+
+        // }
+        // CharAt() & length()
+        String s = "chai and teA";
+        System.out.println(s.charAt(5)); // starts from 0 and also counts ( ) spaces
+
+        // 
+
+    }
+}
