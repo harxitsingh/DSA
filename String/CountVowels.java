25eec9d8166786b267640171747264371172e585
Harshit Singh <harxitsingh@gmail.com>
2026-02-02 22:21:37 +0530

    vowel counting in CountVowels class
    Count the number of vowels in a given string and print them.

--- String/CountVowels.java
+++ String/CountVowels.java
@@ -1,7 +1,20 @@
 public class CountVowels {
     public static void main(String[] args) {
-        String s ="harshit";
-        int count = 0 ;
-        for (int i = 0 ; i<s.length )
+        String s = "harshit";
+        int count = 0;
+        String vowels = ""; // to print vowels also
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+                vowels = vowels + ch; // adding ch
+            }
+        }
+
+        System.out.println(count);
+        System.out.println("Vowels: " + vowels);
+
     }
 }
