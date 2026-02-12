31d916ebe51c4d4712632ee92c3d016c1dc3932f
Harshit Singh <harxitsingh@gmail.com>
2026-02-12 17:55:06 +0530

    leetcode q no 283

---
+++ String/Anagram.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class Anagram {
+    // leetcode q no 242 VALID ANAGRAM - same letters but diff. words (race & care )
+     public static boolean isAnagram(String s1, String s2) {
+
+        if (s1.length() != s2.length())
+            return false;
+
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "car";
+        String s2 = "race";
+
+        boolean result = isAnagram(s1, s2);
+        System.out.println(result); 
+    }
+}
