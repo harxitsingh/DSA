6637cf1958a04400fa407ace3d53b3c3e108c891
Harshit Singh <harxitsingh@gmail.com>
2026-02-17 20:14:47 +0530

    Update SortedSquare_2.java

--- Array/SortedSquare_2.java
+++ Array/SortedSquare_2.java
@@ -6,4 +6,10 @@ class Solution {
         int left = 0;
         int right = n - 1;
         int index = n - 1;
+
+        
+        while(left <= right){
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+
       
