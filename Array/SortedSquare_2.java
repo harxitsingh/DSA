81b86a0bddad6a67141df476224f2027ba953fcf
Harshit Singh <harxitsingh@gmail.com>
2026-02-17 20:14:31 +0530

    Update SortedSquare_2.java

--- Array/SortedSquare_2.java
+++ Array/SortedSquare_2.java
@@ -1,3 +1,9 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
+         int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
       
