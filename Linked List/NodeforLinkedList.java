d321c5181323621c430a78a33266d5d05e34b1b0
Harshit Singh <harxitsingh@gmail.com>
2026-03-28 21:04:42 +0530

    Add Node class and main method for linked list
    Implemented a Node class for a linked list with a main method to demonstrate its usage.

--- Linked List/NodeforLinkedList.java
+++ Linked List/NodeforLinkedList.java
@@ -1,1 +1,27 @@
- hghj
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+
+        Node a = new Node(10); // head node 
+        Node b = new Node(20);
+        Node c = new Node(30);
+
+        a.next = b;
+        b.next = c;
+
+         System.out.println(a.val); // 10
+        System.out.println(a.next.val); // 20
+        System.out.println(a.next);  //output - Node@251a69d7
+        System.out.println(b); // Node@251a69d7  |Adress
+        
+    }
+}
+//del *.class - to delete class 
